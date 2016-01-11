405bbae7545788ff294426e09d338abdfd24621b
Blu3wolf <blue@blu3wolf.com>
2016-01-11 19:39:14 +0800

    add crushed copper ore

--- prototypes/items/metallurgy.lua
+++ prototypes/items/metallurgy.lua
@@ -1,6 +1,6 @@
   data:extend(
 {
--- CRUSHED IRON ORE
+-- CRUSHED ORE
 	{
 		type = "item",
 		name = "crushed-iron-ore",
@@ -10,4 +10,13 @@
 		order = "b[crushed-iron-ore]",
 		stack_size = 100
 	},
+	{
+		type = "item",
+		name = "crushed-copper-ore",
+		icon = "__HARDmod__/placeholder-graphics/icons/plate/crushed-copper-ore.png",
+		flags = {"goes-to-main-inventory"},
+		subgroup = "tf-raw-materials",
+		order = "b[crushed-copper-ore]",
+		stack_size = 100
+	}
 })
--- prototypes/recipes/metallurgy.lua
+++ prototypes/recipes/metallurgy.lua
@@ -11,4 +11,14 @@ data:extend(
 		result_count = 2,
 		enabled = "false"
 	},
+	{
+		type = "recipe",
+		name = "crushed-copper-ore",
+		category = "treefarm-mod-crushing",
+		energy_required = 5,
+		ingredients = {{"copper-ore",1}},
+		result = "crushed-copper-ore",
+		result_count = 2,
+		enabled = "false"
+	},
 })
