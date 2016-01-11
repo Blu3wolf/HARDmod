cdd768a50ddc6ecca313cf107721269292c7d01b
Blu3wolf <blue@blu3wolf.com>
2016-01-11 19:58:49 +0800

    add froth floatation entity
    also moves a bunch of prototype files around

--- prototypes/metallurgy.lua
+++ prototypes/metallurgy.lua
@@ -1,6 +1,16 @@
-  data:extend(
+data:extend(
 {
 -- CRUSHED ORE
+	{
+		type = "recipe",
+		name = "crushed-iron-ore",
+		category = "treefarm-mod-crushing",
+		energy_required = 5,
+		ingredients = {{"iron-ore",1}},
+		result = "crushed-iron-ore",
+		result_count = 2,
+		enabled = "false"
+	},
 	{
 		type = "item",
 		name = "crushed-iron-ore",
@@ -9,6 +19,16 @@
 		subgroup = "tf-raw-materials",
 		order = "b[crushed-iron-ore]",
 		stack_size = 100
+	},
+	{
+		type = "recipe",
+		name = "crushed-copper-ore",
+		category = "treefarm-mod-crushing",
+		energy_required = 5,
+		ingredients = {{"copper-ore",1}},
+		result = "crushed-copper-ore",
+		result_count = 2,
+		enabled = "false"
 	},
 	{
 		type = "item",
