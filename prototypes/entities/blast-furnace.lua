02e5315631352112b2bf049c105e7f81918c7dd2
Blu3wolf <blue@blu3wolf.com>
2016-01-11 17:20:25 +0800

    Fixed node level of fluid boxes for blast furnace

--- prototypes/entities/blast-furnace.lua
+++ prototypes/entities/blast-furnace.lua
@@ -3,7 +3,7 @@ data:extend(
 	{
 		type = "assembling-machine",
 		name = "blast-furnace",
-		flags = {"placeable-neutral", "player-creation"},
+		flags = {"placeable-neutral", "placeable-player", "player-creation"},
 		collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
 		selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
 		minable = 
@@ -14,6 +14,8 @@ data:extend(
 		},
 		crafting_categories = { "blast-furnace" },
 		max_health = 150,
+		corpse = "big-remnants",
+		dying_explosion = "medium-explosion",
 		ingredient_count = 3,
 		energy_usage = "300kW",
 		crafting_speed = 1,
@@ -35,49 +37,51 @@ data:extend(
 		},
 		fluid_boxes = 
 		{
-			production_type = "output",
-			base_area = 10,
-			base_level = 1,
-			pipe_connections = {{ type="output", position = {-3, 1} }}
-      }, 
-	  {
-			production_type = "output",
-			base_area = 10,
-			base_level = 1,
-			pipe_connections = {{ type="output", position = {-3, -1} }}
-      }, 
-      {
-			production_type = "output",
-			base_level = 1,
-			pipe_connections = {{ type="output", position = {3,1} }}
-      }, 
-	  {
-			production_type = "output",
-			base_level = 1,
-			pipe_connections = {{ type="output", position = {3,-1} }}
-      }, 
-      {
-			production_type = "input",
-			base_area = 10,
-			base_level = -1,
-			pipe_connections = {{ type="input", position = {1, -3} }}
-      }, 
-	  {
-			production_type = "input",
-			base_area = 10,
-			base_level = -1,
-			pipe_connections = {{ type="input", position = {-1, -3} }}
-      }, 
-      {
-			production_type = "input",
-			base_level = -1,
-			pipe_connections = {{ type="input", position = {1,3} }}
-      }, 
-	  {
-			production_type = "input",
-			base_level = -1,
-			pipe_connections = {{ type="input", position = {-1,3} }}
-      },
+			{
+				production_type = "output",
+				base_area = 10,
+				base_level = 1,
+				pipe_connections = {{ type="output", position = {-3, 1} }}
+			}, 
+			{
+				production_type = "output",
+				base_area = 10,
+				base_level = 1,
+				pipe_connections = {{ type="output", position = {-3, -1} }}
+			}, 
+			{
+				production_type = "output",
+				base_level = 1,
+				pipe_connections = {{ type="output", position = {3,1} }}
+			}, 
+			{
+				production_type = "output",
+				base_level = 1,
+				pipe_connections = {{ type="output", position = {3,-1} }}
+			}, 
+			{
+				production_type = "input",
+				base_area = 10,
+				base_level = -1,
+				pipe_connections = {{ type="input", position = {1, -3} }}
+			}, 
+			{
+				production_type = "input",
+				base_area = 10,
+				base_level = -1,
+				pipe_connections = {{ type="input", position = {-1, -3} }}
+			}, 
+			{
+				production_type = "input",
+				base_level = -1,
+				pipe_connections = {{ type="input", position = {1,3} }}
+			}, 
+			{
+				production_type = "input",
+				base_level = -1,
+				pipe_connections = {{ type="input", position = {-1,3} }}
+			}
+		}
 	},
 	
 	{
