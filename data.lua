39e4eccecf3e45dd666d785d4f02d3086ab1d80a
Blu3wolf <blue@blu3wolf.com>
2016-01-11 13:55:01 +0800

    Add treefarm config

---
+++ data.lua
@@ -0,0 +1,1 @@
+require("prototypes.treefarmconfig.lua")
---
+++ prototypes/treefarmconfig.lua
@@ -0,0 +1,38 @@
+data.raw.technology["tf-fertilizer"].effects = {
+	{
+		type = "unlock-recipe",
+		recipe = "tf-biolab"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-liquid-air"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-liquid-nitrogen"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-fill-liquid-nitrogen-barrel"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-empty-liquid-nitrogen-barrel"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-phosphate"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-potassium"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-fertilizer1"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "tf-fertilizer2"
+	}
+}
