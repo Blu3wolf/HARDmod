793e1b0caa7174c6418ed4ac70f3a42c6253c000
Blu3wolf <blue@blu3wolf.com>
2016-01-12 18:57:56 +0800

    Remove dytech-gems

--- data.lua
+++ data.lua
@@ -4,5 +4,6 @@ require("prototypes.blast-furnace")
 require("prototypes.forge")
 require("prototypes.treefarmconfig")
 require("prototypes.bobsconfig")
+require("prototypes.dytech-config")
 
 data.raw["player"]["player"].crafting_categories = {"in-hand-crafting"}
---
+++ prototypes/dytech-config.lua
@@ -0,0 +1,28 @@
+-- this removes the dytech gems item category
+
+data.raw["item-subgroup"]["dytech-gem-crystal"] = nil
+data.raw["item-subgroup"]["dytech-gem-raw"] = nil
+data.raw["item-subgroup"]["dytech-gem-cut"] = nil
+data.raw["item-subgroup"]["dytech-gem-compressed"] = nil
+data.raw["item-group"]["dytech-gem"] = nil
+
+-- this removes all the dytech items that needed that category
+
+data.raw.item.crystal = nil
+data.raw.item["raw-ruby"] = nil
+data.raw.item["raw-sapphire"] = nil
+data.raw.item["raw-emerald"] = nil
+data.raw.item["raw-topaz"] = nil
+data.raw.item["raw-diamond"] = nil
+data.raw.item["cut-ruby"] = nil
+data.raw.item["cut-sapphire"] = nil
+data.raw.item["cut-emerald"] = nil
+data.raw.item["cut-topaz"] = nil
+data.raw.item["cut-diamond"] = nil
+data.raw.item["compressed-ruby"] = nil
+data.raw.item["compressed-sapphire"] = nil
+data.raw.item["compressed-emerald"] = nil
+data.raw.item["compressed-topaz"] = nil
+data.raw.item["compressed-diamond"] = nil
+
+
