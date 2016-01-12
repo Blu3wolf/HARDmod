f919d207c64b32362a9361817ce2a49f9b09f6ee
Blu3wolf <blue@blu3wolf.com>
2016-01-12 19:54:41 +0800

    stripped dytech gems

--- prototypes/dytech-config.lua
+++ prototypes/dytech-config.lua
@@ -3,12 +3,12 @@
 data.raw["item-subgroup"]["dytech-gem-crystal"] = nil
 data.raw["item-subgroup"]["dytech-gem-raw"] = nil
 data.raw["item-subgroup"]["dytech-gem-cut"] = nil
-data.raw["item-subgroup"]["dytech-gem-compressed"] = nil
+data.raw["item-subgroup"]["dytech-gem-compressed"] = nil 
 data.raw["item-group"]["dytech-gem"] = nil
 
 -- this removes all the dytech items that needed that category
 
-data.raw.item.crystal = nil
+data.raw.item["crystal"] = nil
 data.raw.item["raw-ruby"] = nil
 data.raw.item["raw-sapphire"] = nil
 data.raw.item["raw-emerald"] = nil
@@ -25,4 +25,34 @@ data.raw.item["compressed-emerald"] = nil
 data.raw.item["compressed-topaz"] = nil
 data.raw.item["compressed-diamond"] = nil
 
+data.raw.recipe["crystal-ruby"] = nil
+data.raw.recipe["crystal-sapphire"] = nil
+data.raw.recipe["crystal-emerald"] = nil
+data.raw.recipe["crystal-topaz"] = nil
+data.raw.recipe["crystal-diamond"] = nil
+data.raw.recipe["raw-cut-ruby"] = nil
+data.raw.recipe["raw-cut-sapphire"] = nil
+data.raw.recipe["raw-cut-emerald"] = nil
+data.raw.recipe["raw-cut-topaz"] = nil
+data.raw.recipe["raw-cut-diamond"] = nil
+data.raw.recipe["cut-compressed-ruby"] = nil
+data.raw.recipe["cut-compressed-sapphire"] = nil
+data.raw.recipe["cut-compressed-emerald"] = nil
+data.raw.recipe["cut-compressed-topaz"] = nil
+data.raw.recipe["cut-compressed-diamond"] = nil
+
+data.raw.resource["gems"] = nil
+
+data.raw.recipe["logic-diamond"] = nil
+data.raw.recipe["logic-diamond-processor"] = nil
+data.raw.technology["logic-diamond-processor"] = nil
+
+data.raw.recipe["coal-ruby"] = nil
+data.raw.recipe["coal-sapphire"] = nil
+data.raw.recipe["coal-emerald"] = nil
+data.raw.recipe["coal-topaz"] = nil
+data.raw.recipe["coal-diamond"] = nil
+
+data.raw.recipe["crystal-sand"] = nil
+data.raw.recipe["crystal-stone"] = nil
 
