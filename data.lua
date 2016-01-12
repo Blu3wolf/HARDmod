7da83bfb9900c1538fc2687828d1a914caa4c5f7
Blu3wolf <blue@blu3wolf.com>
2016-01-12 09:51:05 +0800

    fixes inventory limit

--- data.lua
+++ data.lua
@@ -6,4 +6,3 @@ require("prototypes.treefarmconfig")
 require("prototypes.bobsconfig")
 
 data.raw["player"]["player"].crafting_categories = {"in-hand-crafting"}
-data.raw["player"]["player"].inventory_size = 20
