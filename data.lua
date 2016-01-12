65758199e7e3042cd6dbb7e716c8e6c5343c6461
Blu3wolf <blue@blu3wolf.com>
2016-01-12 19:05:55 +0800

    remove git crap

--- data.lua
+++ data.lua
@@ -3,13 +3,8 @@ require("prototypes.base-replace")
 require("prototypes.metallurgy")
 require("prototypes.blast-furnace")
 require("prototypes.forge")
-<<<<<<< HEAD
 require("prototypes.treefarm-config")
 require("prototypes.bobs-config")
-=======
-require("prototypes.treefarmconfig")
-require("prototypes.bobsconfig")
 require("prototypes.dytech-config")
->>>>>>> Dytech-config
 
 data.raw["player"]["player"].crafting_categories = {"in-hand-crafting"}
