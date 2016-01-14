d6081f7e854c801946e71ea4e041820ded895aff
Blu3wolf <blue@blu3wolf.com>
2016-01-14 15:14:36 +0800

    added tabs from NARMod

--- prototypes/categories.lua
+++ prototypes/categories.lua
@@ -11,6 +11,132 @@ data:extend(
 		order = "e"
 	},
 	
+		-- POWER TAB
+
+	{
+    type = "item-group",
+    name = "power",
+    order = "c-m",
+    inventory_order = "d-m",
+    icon = "__HARDmod__/placeholder-graphics/icons/technology/power-group.png",
+  },
+  
+      {
+    type = "item-subgroup",
+    name = "power-source",
+    group = "power",
+    order = "a"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "power-lamp",
+    group = "power",
+    order = "b"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "power-fuels",
+    group = "power",
+    order = "c"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "power-lines",
+    group = "power",
+    order = "d"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "power-nuclear-1",
+    group = "power",
+    order = "e"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "power-nuclear-2",
+    group = "power",
+    order = "f"
+  },
+  
+    -- FLUIDS / CHEMICALS / ELEMENTS TAB
+  
+    {
+    type = "item-group",
+    name = "fluids",
+    order = "c-f",
+    inventory_order = "a-a",
+    icon = "__HARDmod__/placeholder-graphics/icons/technology/fluids.png",
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "chemicals",
+    group = "fluids",
+    order = "a"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "chemicals-microchip",
+    group = "fluids",
+    order = "q"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "evaporation",
+    group = "fluids",
+    order = "z"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "fluids-oil-processing",
+    group = "fluids",
+    order = "m"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "chemical-electrolysis",
+    group = "fluids",
+    order = "n"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "chemical-furnace",
+    group = "fluids",
+    order = "g"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "chemistry",
+    group = "fluids",
+    order = "g"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "liquid-to-gas",
+    group = "fluids",
+    order = "b"
+  },
+  
+  {
+    type = "item-subgroup",
+    name = "gasses",
+    group = "fluids",
+    order = "a"
+  },
+  
+	
 	--RAW MATERIALS TAB
 	{
 		type = "item-group",
@@ -24,6 +150,24 @@ data:extend(
 		name = "raw-ores",
 		group = "raw-materials",
 		order = "a"
+	},
+	{
+		type = "item-subgroup",
+		name = "raw-alloy",
+		group = "raw-materials",
+		order = "b"
+	},
+	{
+		type = "item-subgroup",
+		name = "raw-plates",
+		group = "raw-materials",
+		order = "a"
+	},
+    {
+		type = "item-subgroup",
+		name = "molten-metals",
+		group = "raw-materials",
+		order = "d",
 	},
 	
 	--CRAFTING CATEGORIES
