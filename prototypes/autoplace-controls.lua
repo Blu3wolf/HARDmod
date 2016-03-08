a08724fbb1bba65d3bc4852e36d41c8c3858d759
Blue <blue@blu3wolf.com>
2016-03-08 12:35:55 +0800

    Add autoplace for new ores

---
+++ prototypes/autoplace-controls.lua
@@ -0,0 +1,40 @@
+data:extend(
+{
+  {
+    type = "autoplace-control",
+    name = "chalcocite",
+    richness = true,
+    order = "b-a"
+  }--[[,
+  {
+    type = "autoplace-control",
+    name = "copper-ore",
+    richness = true,
+    order = "b-b"
+  },
+  {
+    type = "autoplace-control",
+    name = "stone",
+    richness = true,
+    order = "b-c"
+  },
+  {
+    type = "autoplace-control",
+    name = "coal",
+    richness = true,
+    order = "b-d"
+  },
+  {
+    type = "autoplace-control",
+    name = "crude-oil",
+    richness = true,
+    order = "b-e"
+  },
+  {
+    type = "autoplace-control",
+    name = "enemy-base",
+    richness = true,
+    order = "d-a"
+  },--]]
+}
+)
